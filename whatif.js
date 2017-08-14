396c331f55a049f688dbe30a66c9dea317ef9298
Jack Somervell <jack.somervell@curtisbanks.co.uk>
2017-08-14 17:02:37 +0100

    cors finally working

--- whatif.js
+++ whatif.js
@@ -15,15 +15,23 @@ $(document).ready(function(){
     return count;
 	}
 
+$.ajaxPrefilter( function (options) {
+  if (options.crossDomain && jQuery.support.cors) {
+    var http = (window.location.protocol === 'http:' ? 'http:' : 'https:');
+    options.url = http + '//cors-anywhere.herokuapp.com/' + options.url;
+    //options.url = "http://cors.corsproxy.io/url=" + options.url;
+  }
+});
 
 	
 
 	$(".what-if").click(function(){
 
-
-
-    	$('.container').append("<div class = 'ajax-loader'></div>");
-  	
+  // 	$.ajax({crossOrigin:true, url: "http://whateverorigin.org/get?url=https://fantasy.premierleague.com/drf/entry/30615/event/1", success: function(result){
+		// 	console.log(result);
+		// 	console.log('hello');
+		// 	return;
+		// }})
 
 			var teamId = $('.team-id').val()
 
