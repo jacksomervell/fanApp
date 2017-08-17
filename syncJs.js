a4bdd1a1f707370924c7ddff7888f06cc5da0e61
Jack Somervell <jack.somervell@curtisbanks.co.uk>
2017-08-17 15:11:28 +0100

    cleanup

--- syncJs.js
+++ syncJs.js
@@ -161,7 +161,7 @@ $(document).ready(function(){
                               }
                           })  
                    }
-                 }
+                 })
 
               //end of fish:
                })
